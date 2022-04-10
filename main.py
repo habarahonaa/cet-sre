dab167cfded282747fab209366844c1bf2587b88
Hollman A. Barahona <habarahonaa@gmail.com>
2022-04-10 14:49:10 -0600

    feat. Added scheduler for backups

--- main.py
+++ main.py
@@ -3,19 +3,10 @@ import schedule
 import sys
 import time
 
-def main():
-    # Run the controller
-    controller()
-    sys.exit(0)
-
-if __name__ == "__main__":
-    main()
-
-'''
 if __name__ == "__main__":
     # Runs the controller function every hour
-    schedule.every().sunday.at("8:00").do(controller.controller)
+    schedule.every().sunday.at("14:47").do(controller)
     while True:
         schedule.run_pending()
         time.sleep(1)
-'''
+        sys.stdout.flush()
