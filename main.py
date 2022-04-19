77cd05e359f82870aefe3630876967ef46f081ce
Hollman A. Barahona <habarahonaa@gmail.com>
2022-04-18 19:17:44 -0600

    feat. Re-added scheduler

--- main.py
+++ main.py
@@ -6,13 +6,8 @@ from gdrive_authentication import google_drive_auth
 from gdrive_list_files import gdrive_listing
 
 if __name__ == "__main__":
-    # Runs the controller function inmediately
-    gdrive_listing(google_drive_auth())
-    # Runs the controller function every sunday at 10:00
-    '''
     schedule.every().sunday.at("10:00").do(controller)
     while True:
         schedule.run_pending()
         time.sleep(1)
         sys.stdout.flush()
-    '''
